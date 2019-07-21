630585d726586271dc0218c55b33b35af5787eab
Alex <dorofeev86@yahoo.com>
2019-07-21 18:34:07 +0200

    First commit

---
+++ client/src/components/User/cart.js
@@ -0,0 +1,155 @@
+import React, { Component } from "react";
+import UserLayout from "../../hoc/user";
+import UserProductBlock from "../utils/User/product_block";
+
+import { connect } from "react-redux";
+import {
+  getCartItems,
+  removeCartItem,
+  onSuccessBuy
+} from "../../actions/user_actions";
+
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import faFrown from "@fortawesome/fontawesome-free-solid/faFrown";
+import faSmile from "@fortawesome/fontawesome-free-solid/faSmile";
+
+/// AYDyeWZAmO9mOo9KX1tIGeUrYRSFAJmc4smYE6E3nG00o31NsIzfGplFGyfwnrQ2Q7c0uT0Sr48ji7Ae
+
+import Paypal from "../utils/paypal";
+
+class UserCart extends Component {
+  state = {
+    loading: true,
+    total: 0,
+    showTotal: false,
+    showSuccess: false
+  };
+
+  componentDidMount() {
+    let cartItems = [];
+    let user = this.props.user;
+
+    if (user.userData.cart) {
+      if (user.userData.cart.length > 0) {
+        user.userData.cart.forEach(item => {
+          cartItems.push(item.id);
+        });
+        this.props
+          .dispatch(getCartItems(cartItems, user.userData.cart))
+          .then(() => {
+            if (this.props.user.cartDetail.length > 0) {
+              this.calculateTotal(this.props.user.cartDetail);
+            }
+          });
+      }
+    }
+  }
+
+  calculateTotal = cartDetail => {
+    let total = 0;
+
+    cartDetail.forEach(item => {
+      total += parseInt(item.price, 10) * item.quantity;
+    });
+
+    this.setState({
+      total,
+      showTotal: true
+    });
+  };
+
+  removeFromCart = id => {
+    this.props.dispatch(removeCartItem(id)).then(() => {
+      if (this.props.user.cartDetail.length <= 0) {
+        this.setState({
+          showTotal: false
+        });
+      } else {
+        this.calculateTotal(this.props.user.cartDetail);
+      }
+    });
+  };
+
+  showNoItemMessage = () => (
+    <div className="cart_no_items">
+      <FontAwesomeIcon icon={faFrown} />
+      <div>You have no items</div>
+    </div>
+  );
+
+  transactionError = data => {
+    console.log("Paypal error");
+  };
+
+  transactionCanceled = () => {
+    console.log("Transaction cancled");
+  };
+
+  transactionSuccess = data => {
+    this.props
+      .dispatch(
+        onSuccessBuy({
+          cartDetail: this.props.user.cartDetail,
+          paymentData: data
+        })
+      )
+      .then(() => {
+        if (this.props.user.successBuy) {
+          this.setState({
+            showTotal: false,
+            showSuccess: true
+          });
+        }
+      });
+  };
+
+  render() {
+    return (
+      <UserLayout>
+        <div>
+          <h1>My cart</h1>
+          <div className="user_cart" >
+            <UserProductBlock
+              products={this.props.user}
+              type="cart"
+              removeItem={id => this.removeFromCart(id)}
+            />
+            {this.state.showTotal ? (
+              <div>
+                <div className="user_cart_sum">
+                  <div>Total amount: $ {this.state.total}</div>
+                </div>
+              </div>
+            ) : this.state.showSuccess ? (
+              <div className="cart_success">
+                <FontAwesomeIcon icon={faSmile} />
+                <div>THANK YOU</div>
+                <div>YOUR ORDER IS NOW COMPLETE</div>
+              </div>
+            ) : (
+              this.showNoItemMessage()
+            )}
+          </div>
+          {this.state.showTotal ? (
+            <div className="paypal_button_container">
+              <Paypal
+                toPay={this.state.total}
+                transactionError={data => this.transactionError(data)}
+                transactionCanceled={data => this.transactionCanceled(data)}
+                onSuccess={data => this.transactionSuccess(data)}
+              />
+            </div>
+          ) : null}
+        </div>
+      </UserLayout>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    user: state.user
+  };
+};
+
+export default connect(mapStateToProps)(UserCart);
---
+++ client/src/routes.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Layout from "./hoc/layout";
+import Auth from "./hoc/auth";
+
+import Register from "./components/Register_login";
+import RegisterLogin from "./components/Register_login/register";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
+import ProductPage from './components/Product';
+
+import UserDashboard from "./components/User";
+import AddProduct from "./components/User/Admin/add_product";
+import ManageCategories from './components/User/Admin/manage_categories';
+import UserCart from './components/User/cart';
+import UpdateProfile from './components/User/update_profile';
+import ManageSite from './components/User/Admin/manage_site';
+
+const Routes = () => {
+  return (
+    <Layout>
+      <Switch>
+        <Route path="/user/dashboard" exact component={Auth(UserDashboard, true)}/>
+        <Route path="/user/cart" exact component={Auth(UserCart, true)}/>
+        <Route  path="/admin/add_product" exact component={Auth(AddProduct, true)}/>
+        <Route  path="/admin/manage_categories" exact component={Auth(ManageCategories, true)}/>
+        {Auth(AddProduct, true)}/>
+        <Route  path="/admin/site_info" exact component={Auth(ManageSite, true)}/>
+        <Route  path="/user/user_profile" exact component={Auth(UpdateProfile, true)}/>
+        <Route path="/product_detail/:id" exact component={Auth(ProductPage, null)} />
+        <Route path="/register" exact component={Auth(RegisterLogin, false)} />
+        <Route path="/register_login" exact component={Auth(Register, false)} />
+        <Route path="/shop" exact component={Auth(Shop, null)} />
+        <Route path="/" exact component={Auth(Home, null)} />
+        />
+      </Switch>
+    </Layout>
+  );
+};
+export default Routes;
