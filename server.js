e03844d9a245863250146efb0b8272778c45e138
Alex <dorofeev86@yahoo.com>
2019-07-22 10:24:15 +0200

    Changes in server

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const mongoose = require("mongoose");
 const async = require("async");
 require("dotenv").config();
 
-mongoose.Promise = global.Promise;
+mongoose.set("useCreateIndex", true);
 
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true })
